d7e469276cd973d744f7e1768288fdac8dffa1cc
joshua-t-liu <josh.th.liu@gmail.com>
2020-09-06 12:53:26 -0700

    refactored code

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-const url = 'https://www.gazelle.com/iphone/';
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+const PHONE = 4;
+const CARRIER = 5;
+const DEVICE = 6;
+const ID = 7;
+
+const getPhoneDetails = function(link) {
+  if (typeof link !== 'string') return null;
+  let split = link.split('/');
+  if (split.length !== 8) return null;
+  return (
+    split[PHONE] + ',' +
+    split[CARRIER] + ',' +
+    split[DEVICE] + ',' +
+    split[ID]
+  );
+};
+
+const pullPrice = async function(page, links) {
+  let deviceLinks = links;
+
+  if (!deviceLinks) {
+    let devices = await fsPromises.readFile('devices.csv', { encoding: 'utf8' });
+    devices = devices.split('\n');
+    devices.forEach((device, idx) => {
+      if (device.length) deviceLinks.push(device);
+    });
+  }
+
+  const results = [];
+  for (let i=0; i<deviceLinks.length; i++) {
+    await page.goto(deviceLinks[i]);
+    for (let i=1; i<=3; i++) {
+      if (i === 1) {
+        await page.click(`div[name="QuestionWithoutResponseScroll"]:nth-of-type(${i}) > div > div > div.option:nth-child(1) > button`);
+      } else {
+        await page.click(`div[name="QuestionWithoutResponseScroll"]:nth-of-type(${i}) > div > div > div.option:nth-child(2) > button`);
+      }
+    };
+    await page.waitFor(750);
+    let val;
+    try {
+      val = await page.$eval('span[aria-labelledby="amount"]', (el) => el.innerText);
+    } catch (error) {
+      console.log('error', deviceLinks[i], error);
+    }
+    results[i] = [deviceLinks[i], val];
+  }
+
+  writer = new fs.WriteStream('./results.csv');
+  results.forEach(([link, val]) => writer.write(getPhoneDetails(link) + ',' + val  + '\n'));
+  writer.end();
+}
+
+const getDeviceLinks = async function (page) {
+  const url = 'https://www.gazelle.com/iphone/';
   await page.goto(url);
-  
-  // get list of phones
   const phones = await page.$$eval('div.transition-container', (divs) => {
     return Array.from(divs[0].children).map((child) => child.children[0].href)
   });
-  
+
   console.log('phones', phones.length);
 
   let carrierLinks = [];
@@ -23,10 +73,10 @@ const url = 'https://www.gazelle.com/iphone/';
     });
     carrierLinks.push(carriers);
   };
-  
+
   carrierLinks = carrierLinks.flat();
   console.log('carriers', carrierLinks.length);
-  
+
   // get device links
   let deviceLinks = [];
   for (let i=0; i<carrierLinks.length; i++) {
@@ -34,7 +84,7 @@ const url = 'https://www.gazelle.com/iphone/';
     const devices = await page.$$eval('ul.products', (el) => {
       return Array.from(el[0].children).map((child) => child.children[0].href);
     });
-    deviceLinks.push(devices); 
+    deviceLinks.push(devices);
   }
 
   deviceLinks = deviceLinks.flat();
@@ -43,32 +93,17 @@ const url = 'https://www.gazelle.com/iphone/';
   let writer = new fs.WriteStream('./devices.csv');
   deviceLinks.forEach((device) => writer.write(device + '\n'));
   writer.end();
-  
-  const results = [];
-  for (let i=0; i<deviceLinks.length; i++) {
-    await page.goto(deviceLinks[i]);
-    for (let i=1; i<=3; i++) {
-      if (i === 1) {
-        await page.click(`div[name="QuestionWithoutResponseScroll"]:nth-of-type(${i}) > div > div > div.option:nth-child(1) > button`);
-      } else {
-        await page.click(`div[name="QuestionWithoutResponseScroll"]:nth-of-type(${i}) > div > div > div.option:nth-child(2) > button`);
-      }
-    };
-    await page.waitFor(500);
-    let val;
-    try {
-      val = await page.$eval('span[aria-labelledby="amount"]', (el) => el.innerText);
-    } catch (error) {
-      console.log('error', deviceLinks[i],error);
-    }
-    results[i] = [deviceLinks[i], val];
-  }
-  
-  console.log(results);
+};
 
-  writer = new fs.WriteStream('./results.csv');
-  results.forEach(([link, val]) => writer.write(link + ',' + val  + '\n'));
-  writer.end();
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // get list of devices
+  // await getDeviceLinks(page);
+
+  // pull phone market price
+  await pullPrice(page, deviceLinks);
 
   await browser.close();
 })();
