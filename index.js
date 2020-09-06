51cfe518904fa924b945679bce96b8fc70733e52
Ubuntu <ubuntu@ip-172-31-54-126.ec2.internal>
2020-09-06 07:24:59 +0000

    test gazelle connection

--- index.js
+++ index.js
@@ -8,5 +8,15 @@ const url = 'https://www.gazelle.com/iphone';
   await page.goto(url);
   await page.screenshot({path: 'example.png'});
 
+  const phones = await page.$$eval('div', (divs) => divs);
+  console.log(phones);
+
+  
+  //await page.goto('https://news.ycombinator.com/news')
+
+  // execute standard javascript in the context of the page.
+  // const stories = await page.$$eval('a.storylink', anchors => { return anchors.map(anchor => anchor.textContent).slice(0, 10) })
+  // console.log(stories)
+
   await browser.close();
 })();
